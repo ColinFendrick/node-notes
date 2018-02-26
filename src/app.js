c4aeb296639b0dfb4ab8d1adb57ce75b5c602ba2
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-26 11:08:45 -0500

    Fixed app

--- src/app.js
+++ src/app.js
@@ -1,53 +1,7 @@
-const greet = require("./greet");
+var english = require("./english");
+var spanish = require("./spanish");
 
-// greet();
-
-// const person = {
-// 	name: {
-// 		first: "John",
-// 		last: "Doe"
-// 	},
-// 	greet: () => {
-// 		console.log(`Hello, ${person.name.first} ${person.name.last}`);
-// 	}
-// };
-
-// person.greet();
-
-// function Person(firstname, lastname) {
-// 	this.firstname = firstname;
-// 	this.lastname = lastname;
-// }
-
-// Person.prototype.greet = function() {
-
-// 	console.log(`Hello, ${this.firstname} ${this.lastname}`);
-// };
-
-// const john = new Person("John", "Doe");
-// john.greet();
-
-//by value
-const change = b => {
-	b = 2;
+module.exports = {
+	english: english,
+	spanish: spanish
 };
-
-const a = 1;
-change(a);
-console.log(a);
-
-//by reference
-function changeObj(d) {
-	d.prop1 = () => {};
-	d.prop2 = {};
-}
-
-const c = {};
-c.prop1 = {};
-changeObj(c);
-console.log(c);
-
-//immediately invoked function expression
-(function (params) {
-	console.log("this is how we used to fake modules", params);
-}("params"));
