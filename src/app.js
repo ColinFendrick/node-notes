9c3b0e98cab431d7d76870d5fa7d2480f886555c
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-06 13:43:40 -0500

    A few basic notes about revealing module patterns

--- src/app.js
+++ src/app.js
@@ -8,10 +8,13 @@ var greet3 = require("./greet3");
 greet3.greet();
 greet3.greeting = "Changed hello world!";
 
+// This will call "Changed hello world!" since Node is caching the modules by the filename
+// so if I alter the greet3 objet, it will be altered every time it is called
 var greet3b = require("./greet3");
 greet3b.greet();
 
 var Greet4 = require("./greet4");
+// I have to create a new instance of this object, since I am only exporting the constructor fn, not an instance of the object in Greet4
 var grtr = new Greet4();
 grtr.greet();
 
--- src/greet5.js
+++ src/greet5.js
@@ -4,6 +4,7 @@ function greet() {
 	console.log(greeting);
 }
 
+// Common way to export, only exposing the function, not the variable
 module.exports = {
 	greet: greet
 };
