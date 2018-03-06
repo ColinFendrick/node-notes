864f6b6f04d05ce97886b1f08f2f044a4a693250
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-06 15:37:48 -0500

    Using Nodes emitter

--- src/deprecated-greet/app-emitter.js
+++ src/deprecated-greet/app-emitter.js
@@ -1,13 +1,13 @@
-var Emitter = require("events");
+var Emitter = require("./emitter");
 
 var emtr = new Emitter();
 
 // Creating listeners
-emtr.on("greet", function () {
+emtr.on("greet", function() {
 	console.log("Someone said hello");
 });
 
-emtr.on("greet", function () {
+emtr.on("greet", function() {
 	console.log("A greeting occcured!");
 });
 
