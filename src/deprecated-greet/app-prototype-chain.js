b15fa85e55340a8d6dbaac638e35e6cf52d9b194
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-07 07:44:24 -0500

    prototype chain stuff

--- src/deprecated-greet/app-prototype-chain.js
+++ src/deprecated-greet/app-prototype-chain.js
@@ -7,7 +7,7 @@ function Greetr() {
 
 util.inherits(Greetr, EventEmitter);
 
-Greetr.prototype.greet = function(data) {
+Greetr.prototype.greet = function (data) {
 	console.log(this.greeting + ": " + data);
 	this.emit("greet", data);
 };
